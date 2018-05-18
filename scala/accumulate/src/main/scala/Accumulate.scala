e20168cf3a2674a92948d980f51778eb5f3c56ec
tmukh3 <tirthankar.mukherjee2@nike.com>
2018-05-18 16:14:21 -0700

    accumulate

--- scala/accumulate/src/main/scala/Accumulate.scala
+++ scala/accumulate/src/main/scala/Accumulate.scala
@@ -1,3 +1,18 @@
+//for .. yeild is just cinverted to a map
+// class Accumulate {
+//   def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = {
+//     for(value <- list) yield f(value)
+//   }
+// }
+
+
 class Accumulate {
-  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = ???
+  def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = {
+    tailRecursion(f, list, List())
+  }
+
+  def tailRecursion[A, B](f: (A) => B, list : List[A], accumulatedList: List[B]) : List[B] = {
+    if(list.isEmpty) return accumulatedList;
+    else return tailRecursion(f, list.tail,  accumulatedList :+ f(list.head))
+  }
 }
