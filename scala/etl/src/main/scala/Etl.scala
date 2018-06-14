ed42dd75fdc5a49a86e1e903446bb759c6c88c10
tmukh3 <tirthankar.mukherjee2@nike.com>
2018-06-14 08:50:10 -0700

    updates ETL

--- scala/etl/src/main/scala/Etl.scala
+++ scala/etl/src/main/scala/Etl.scala
@@ -3,7 +3,6 @@ object Etl {
 
   def transform(scores: Map[Int, Seq[String]]): Map[String, Int] = {
     var returnMap = Map[String, Int]()
-    //  scores.map(x => x.values.map(y => returnMap + (y.toLowerCase -> x.key)))
     scores map { case (key, value) => (value.map(x => (returnMap += (x.toLowerCase -> key)))) }
 
     return returnMap
