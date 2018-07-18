9acf6005cac9eb94c76e7fd214d5731d2371fd8e
tmukh3 <tirthankar.mukherjee2@nike.com>
2018-07-18 12:02:11 -0700

    Perfectnumber 2

--- scala/perfect-numbers/src/main/scala/PerfectNumbers.scala
+++ scala/perfect-numbers/src/main/scala/PerfectNumbers.scala
@@ -6,20 +6,15 @@ object PerfectNumbers {
   def classify(n : Int) : Either[String, NumberType] = {
     if(n <= 0 ) return Left("Classification is only possible for natural numbers.")
 
-    factors(n,1, List[Int]()).sum match {
+  //  val seq:Seq[Int]  = for ( f <- 1 to n/2 if(n % f) == 0 ) yield f
+    (1 to n/2).filter(n % _ == 0).sum match {
       case x if x > n => Right(NumberType.Abundant)
       case x if x == n => Right(NumberType.Perfect)
       case x if x < n => Right(NumberType.Deficient)
     }
   }
 
-  def factors(n : Int, f: Int, list : List[Int]) : List[Int] = {
-    if((f-1) >= (n / 2)) return list
-    (n % f) match {
-      case 0 => factors( n, f + 1, f :: list)
-      case _ => factors( n, f + 1, list)
-    }
-  }
+
 }
 
 object NumberType extends Enumeration {
